6bd6345f057d3d591987d6b5d9a148a8fac8e010
Kjell Konis <kjellpk@users.noreply.github.com>
2009-11-15 16:39:17 +0000

    Tests now use demo code to build networks.

--- tests/simpleGaussianExample.R
+++ tests/simpleGaussianExample.R
@@ -1,24 +1,5 @@
 library(RHugin, lib.loc = "../")
-simpleGaussianExample <- hugin.domain()
-
-add.node(simpleGaussianExample, "X", kind = "continuous")
-add.node(simpleGaussianExample, "Y", kind = "continuous")
-add.node(simpleGaussianExample, "Z", kind = "continuous")
-
-add.edge(simpleGaussianExample, "Y", "X")
-add.edge(simpleGaussianExample, "Z", "Y")
-
-X.table <- get.table(simpleGaussianExample, "X")
-X.table[["Value"]] <- c(0, 1)
-set.table(simpleGaussianExample, "X", X.table)
-
-Y.table <- get.table(simpleGaussianExample, "Y")
-Y.table[["Value"]] <- c(0, 1, 1)
-set.table(simpleGaussianExample, "Y", Y.table)
-
-Z.table <- get.table(simpleGaussianExample, "Z")
-Z.table[["Value"]] <- c(0, 1, 1)
-set.table(simpleGaussianExample, "Z", Z.table)
+demo("buildSimpleGaussianNetwork", echo = FALSE)
 
 compile(simpleGaussianExample)
 set.finding(simpleGaussianExample, "Z", 1.5)
