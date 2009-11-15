98a354340541a337502c2fffbebb54122a904dca
kjell <kjell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-15 16:39:17 +0000

    Tests now use demo code to build networks.

--- trunk/tests/appleTree.R
+++ trunk/tests/appleTree.R
@@ -1,24 +1,5 @@
 library(RHugin, lib.loc = "../")
-AppleTree <- hugin.domain()
-
-add.node(AppleTree, "Sick", states = c("yes", "no"))
-add.node(AppleTree, "Dry", states = c("yes", "no"))
-add.node(AppleTree, "Loses", states = c("yes", "no"))
-
-add.edge(AppleTree, "Loses", "Sick")
-add.edge(AppleTree, "Loses", "Dry")
-
-Sick.table <- get.table(AppleTree, "Sick")
-Sick.table[["Freq"]] <- c(0.1, 0.9)
-set.table(AppleTree, "Sick", Sick.table)
-
-Dry.table <- get.table(AppleTree, "Dry")
-Dry.table[["Freq"]] <- c(0.1, 0.9)
-set.table(AppleTree, "Dry", Dry.table)
-
-Loses.table <- get.table(AppleTree, "Loses")
-Loses.table[["Freq"]] <- c(0.95, 0.05, 0.85, 0.15, 0.90, 0.1, 0.02, 0.98)
-set.table(AppleTree, "Loses", Loses.table)
+demo("buildAppleTreeNetwork", echo = FALSE)
 
 compile(AppleTree)
 set.finding(AppleTree, "Loses", "yes")
--- trunk/tests/asia.R
+++ trunk/tests/asia.R
@@ -1,64 +1,20 @@
 library(RHugin, lib.loc = "../")
-asia <- hugin.domain()
+demo("buildChestClinicNetwork", echo = FALSE)
 
-nodes <- c("Asia", "Smoking", "Tuberculosis", "LungCancer",
-            "Bronchitis", "Either", "XRay", "Dyspnoea")
-for(node in nodes)
-  add.node(asia, node, states = c("yes", "no"))
+compile(ChestClinic)
 
-add.edge(asia, "Tuberculosis", "Asia")
-add.edge(asia, "LungCancer", "Smoking")
-add.edge(asia, "Bronchitis", "Smoking")
-add.edge(asia, "Either", c("Tuberculosis", "LungCancer"))
-add.edge(asia, "XRay", "Either")
-add.edge(asia, "Dyspnoea", c("Either", "Bronchitis"))
+set.finding(ChestClinic, "Asia", "yes")
+set.finding(ChestClinic, "Dyspnoea", "yes")
 
-Asia.table <- get.table(asia, "Asia")
-Asia.table[["Freq"]] <- c(0.01, 0.99)
-set.table(asia, "Asia", Asia.table)
+propagate(ChestClinic)
 
-Smoking.table <- get.table(asia, "Smoking")
-Smoking.table[["Freq"]] <- c(0.5, 0.5)
-set.table(asia, "Smoking", Smoking.table)
-
-Tuberculosis.table <- get.table(asia, "Tuberculosis")
-Tuberculosis.table[["Freq"]] <- c(0.05, 0.95, 0.01, 0.99)
-set.table(asia, "Tuberculosis", Tuberculosis.table)
-
-LungCancer.table <- get.table(asia, "LungCancer")
-LungCancer.table[["Freq"]] <- c(0.1, 0.9, 0.01, 0.99)
-set.table(asia, "LungCancer", LungCancer.table)
-
-Bronchitis.table <- get.table(asia, "Bronchitis")
-Bronchitis.table[["Freq"]] <- c(0.6, 0.4, 0.3, 0.7)
-set.table(asia, "Bronchitis", Bronchitis.table)
-
-Either.table <- get.table(asia, "Either")
-Either.table[["Freq"]] <- c(1, 0, 1, 0, 1, 0, 0, 1)
-set.table(asia, "Either", Either.table)
-
-XRay.table <- get.table(asia, "XRay")
-XRay.table[["Freq"]] <- c(0.98, 0.02, 0.05, 0.95)
-set.table(asia, "XRay", XRay.table)
-
-Dyspnoea.table <- get.table(asia, "Dyspnoea")
-Dyspnoea.table[["Freq"]] <- c(0.9, 0.1, 0.8, 0.2, 0.7, 0.3, 0.1, 0.9)
-set.table(asia, "Dyspnoea", Dyspnoea.table)
-
-compile(asia)
-
-set.finding(asia, "Asia", "yes")
-set.finding(asia, "Dyspnoea", "yes")
-
-propagate(asia)
-
-get.belief(asia, "Asia")
-get.belief(asia, "Smoking")
-get.belief(asia, "Tuberculosis")
-get.belief(asia, "LungCancer")
-get.belief(asia, "Bronchitis")
-get.belief(asia, "Either")
-get.belief(asia, "XRay")
-get.belief(asia, "Dyspnoea")
+get.belief(ChestClinic, "Asia")
+get.belief(ChestClinic, "Smoking")
+get.belief(ChestClinic, "Tuberculosis")
+get.belief(ChestClinic, "LungCancer")
+get.belief(ChestClinic, "Bronchitis")
+get.belief(ChestClinic, "Either")
+get.belief(ChestClinic, "XRay")
+get.belief(ChestClinic, "Dyspnoea")
 
 
