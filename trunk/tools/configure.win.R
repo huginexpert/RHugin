95c309f63a53007ca7fedc55d22ad5420f5dee6a
kjell <kjell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-01-20 22:42:04 +0000

    Small fixes to preliminary Windows source package support.

--- trunk/tools/configure.win.R
+++ trunk/tools/configure.win.R
@@ -1,62 +1,89 @@
 rm(list = ls())
-fsep <- .Platform$file.sep #"\\"
+library(tools)
+
 compilerID <- "vc10"
 
-heDir <- file.path(Sys.getenv("PROGRAMFILES"), "Hugin Expert", fsep = fsep)
-hugins <- strsplit(list.files(heDir), split = " ", fixed = TRUE)
+if(nchar(hugins <- Sys.getenv("RHUGIN_HUGINHOME"))) {
+  HEDir <- dirname(hugins)
+  hugins <- basename(hugins)
+} else {
+  HEDir <- file.path(Sys.getenv("PROGRAMFILES"), "Hugin Expert")
+  HEDir <- file.path(dirname(HEDir), basename(HEDir))
+  hugins <- list.files(HEDir)
+}
 
+hugins <- strsplit(hugins, split = " ", fixed = TRUE)
 l4 <- sapply(hugins, length) == 4
 isHugin <- sapply(hugins, function(u) u[1]) == "Hugin"
 hugins <- hugins[l4 & isHugin]
 
 if(!length(hugins))
-  stop("Hugin not found in ", heDir)
+  stop("Hugin not found in ", HEDir)
 
-type <- sapply(hugins, function(u) u[2])
-DorR <- type == "Developer" || type == "Researcher"
+types <- sapply(hugins, function(u) u[2])
+DorR <- types == "Developer" || types == "Researcher"
 if(sum(DorR)) {
   hugins <- hugins[DorR]
-  type <- hugins[DorR]
+  types <- types[DorR]
 }
 
 versions <- as.numeric(sapply(hugins, function(u) u[3]))
 latest <- which.max(versions)
 
 hugin <- paste(hugins[[latest]], collapse = " ")
-type <- type[latest]
+type <- types[latest]
 version <- versions[latest]
 
-HuginCHDEDir <- file.path(heDir, hugin, paste("HDE", version, "C", sep = ""), fsep = fsep)
-HuginIncludeDir <- file.path(HuginCHDEDir, "Include", fsep = fsep)
+HuginCHDEDir <- file.path(HEDir, hugin, paste("HDE", version, "C", sep = ""))
+HuginIncludeDir <- file.path(HuginCHDEDir, "Include")
 dllName <- paste("hugin2", version, compilerID, "x64.dll", sep = "-")
 HuginDll <- list.files(HuginCHDEDir, pattern = glob2rx(dllName),
                        recursive = TRUE, full.names = TRUE)
 HuginDllDir <- dirname(HuginDll)
 
-
-#PKG_CPPFLAGS=-DH_DOUBLE -I$(HUGIN_INCLUDE_DIR)
-#PKG_LIBS=-L$(HUGIN_LIB_DIR) -lhugin2 -lm -lz -lpthread $(RHUGIN_LIBCRYPT) \
-#$(HUGIN_RPATH)
-
 if(dir.exists(d <- file.path("src", "HuginDll")))
-  unlink(d, recursive = TRUE)
+  status <- unlink(d, recursive = TRUE)
 
-dir.create(d)
-file.copy(HuginDll, file.path(d, dllName))
+status <- dir.create(d)
+status <- file.copy(HuginDll, file.path(d, dllName))
 
 if(file.exists(m <- file.path("src", "Makevars.win")))
-  file.remove(m)
+  status <- file.remove(m)
 
 cppflags <- paste("PKG_CPPFLAGS=-DH_DOUBLE -I\"",
                   HuginIncludeDir, "\"", sep = "")
 pkglibs <- paste("PKG_LIBS=-L\"", file.path(getwd(), "src", "HuginDll"), "\"",
-                 " -l", "\"", substr(dllName, 1, 19), "\" ", "-lm -lz", sep = "")
+                 " -l", "\"", file_path_sans_ext(dllName), "\" ", "-lm -lz",
+                 sep = "")
 
 writeLines(c(cppflags, pkglibs), con = m)
 
-onLoad <- readLines(ol <- file.path("R", "onLoad.q"))
-i <- grep("###ConfigureHuginDllDirHere###", onLoad)
-onLoad[i] <- paste("    HuginDllDir <- ", "\"", HuginDllDir, "\" ", "###ConfigureHuginDllDirHere###", sep = "")
+onLoad <- readLines(ol <- file.path("R", "onLoad.R"))
+
+i <- grep("###ConfigureHuginVersionHere###", onLoad)[1]
+onLoad[i] <- paste("    HuginVersion <- ",
+                   "\"",
+                   hugin,
+                   "\"",
+                   " ###ConfigureHuginVersionHere###",
+                   sep = "")
+
+i <- grep("###ConfigureHuginDirHere###", onLoad)[1]
+onLoad[i] <- paste("    HuginInst <- ",
+                   "\"",
+                   file.path(HEDir, hugin),
+                   "\" ",
+                   "###ConfigureHuginDirHere###",
+                   sep = "")
+
+i <- grep("###ConfigureHuginDllDirHere###", onLoad)[1]
+onLoad[i] <- paste("    HuginDllDir <- ",
+                   "\"",
+                   HuginDllDir,
+                   "\" ",
+                   "###ConfigureHuginDllDirHere###",
+                   sep = "")
+
 status <- file.remove(ol)
 writeLines(onLoad, con = ol)
 
