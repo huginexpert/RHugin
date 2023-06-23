90563b3c6972542deec82246dfaaa3271ad519db
Mark Christiansen <90259663+HUGIN-MC@users.noreply.github.com>
2023-06-23 14:49:26 +0200

    Update configure.win.R
    - R 4.3 should now be supported
    - Changed vc version from 10 to 17

--- tools/configure.win.R
+++ tools/configure.win.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 library(tools)
 
-compilerID <- "vc10"
+compilerID <- "vc17"
 
 if(nchar(hugins <- Sys.getenv("RHUGIN_HUGINHOME"))) {
   HEDir <- dirname(hugins)
@@ -21,11 +21,11 @@ if(!length(hugins))
   stop("Hugin not found in ", HEDir)
 
 types <- sapply(hugins, function(u) u[2])
-DorR <- types == "Developer" || types == "Researcher"
-if(sum(DorR)) {
-  hugins <- hugins[DorR]
-  types <- types[DorR]
-}
+#DorR <- types == "Developer" || types == "Researcher"
+#if(sum(DorR)) {
+#  hugins <- hugins[DorR]
+#  types <- types[DorR]
+#}
 versions <- (sapply(hugins, function(u) u[2])) #OBS Change u[2] to u[3] if hugin is not found
 #versions <- as.numeric(sapply(hugins, function(u) u[2])) #OBS Change u[2] to u[3] if hugin is not found
 latest <- which.max(versions)
