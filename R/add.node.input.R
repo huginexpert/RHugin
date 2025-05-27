623f2d109a0d331e9ddf359abf03ef16d7579ec3
Steven Tran <steven@hugin.com>
2025-05-27 09:24:32 +0200

    Add class node as input

---
+++ R/add.node.input.R
@@ -0,0 +1,12 @@
+add.node.input <- function(class, nodes) 
+{
+    node.ptrs <- nodePointersFromNames(class, nodes)
+    for (node in nodes) {
+        if(is.null(node.ptrs[[node]]))
+            stop(dQuote(node), "could not be found in the class")
+        else
+            .Call(RHugin_node_add_to_input, node.ptrs[[node]])
+    }
+
+    invisible()
+}
