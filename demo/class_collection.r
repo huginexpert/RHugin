73de33cba53ee51baadf16950e18e1d16f17b425
Steven Tran <steven@hugin.com>
2025-05-27 10:19:34 +0200

    Add WIP class collection example

---
+++ demo/class_collection.r
@@ -0,0 +1,41 @@
+library(RHugin)
+
+# Create a class collection
+cc <- hugin.class.collection()
+
+# Create a class
+class <- hugin.class(cc)
+
+# Set class name
+set.class.name(class, "fruits")
+
+# Get class name
+get.class.name(class)
+
+# Get class by name 
+fruits_class <- hugin.class.collection.get.class.by_name(cc, "fruits")
+
+# Add node to class
+add.class.node(fruits_class, "apple", states = c("yes", "no", "maybe"))
+add.class.node(fruits_class, "banana", states = c("yes", "no", "maybe"))
+add.class.node(fruits_class, "pear", states = c("yes", "no", "maybe"))
+
+# Add node to inputs of its home class
+add.node.input(fruits_class, c("apple", "banana"))
+
+# Get inputs of class
+inputs <- get.class.inputs(class)
+
+# Add node to output of its home class
+
+
+# Add node to outputs of its home class
+write.rhc(fruits_class, "test.oobn")
+
+
+
+
+
+
+
+
--- demo/dbn.r
+++
@@ -1,4 +0,0 @@
-# Create a Hugin domain
-cc <- hugin.class.collection()
-class <- hugin.class(cc)
-dbn <- hugin.domain.dbn(class, 10)
