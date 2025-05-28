423c273f1f50af581229567ada1bf340debfdac1
Steven Tran <steven@hugin.com>
2025-05-28 15:10:13 +0200

    Add instance node can be named

--- R/hugin.class.instance.R
+++ R/hugin.class.instance.R
@@ -1,4 +1,7 @@
-hugin.class.instance <- function(class1, class2) {
-    node.instance <- .Call(RHugin_class_new_instance, class1, class2)
-    node.instance
+hugin.class.instance <- function(class1, class2, name = NULL) {
+    node <- .Call(RHugin_class_new_instance, class1, class2)
+    if(!is.null(name[1]))
+        .Call(RHugin_node_set_name, node, name[1])
+    
+    invisible()
 }
